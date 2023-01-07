ee3097567e8c382bbc8d82a2c8e499b1420626f9
eddde <50966754+eddde@users.noreply.github.com>
2023-01-07 15:55:51 +0100

    Initial ui change

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "Based Ludo (based)",
         native_options,
         Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
     );
